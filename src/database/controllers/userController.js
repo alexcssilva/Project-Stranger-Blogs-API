243460010b813fc22d685871eb2f760798cae3ed
Alex Silva <alexcssilva@gmail.com>
2022-05-23 20:09:59 -0300

    feat: added - getUser - controllers/userController.js - Desafio 05

--- src/database/controllers/userController.js
+++ src/database/controllers/userController.js
@@ -1,17 +1,23 @@
-const { newUser } = require('../services/userService');
+const { newUser, getAllUsers } = require('../services/userService');
 const generateJWT = require('../utils/generateJWT');
 
 const createUser = async (req, res) => {
   const { displayName, email, password, image } = req.body;
-  const user = { email };
-  const token = generateJWT(user);
+  const token = generateJWT(email);
 
-  if (user) {
+  if (email) {
   await newUser({ displayName, email, password, image });
   return res.status(201).json({ token });
   }
 };
 
+const getUser = async (req, res) => {
+  const users = await getAllUsers();
+
+  return res.status(200).json(users);
+};
+
 module.exports = {
   createUser,
+  getUser,
 };

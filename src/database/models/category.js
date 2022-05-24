198f266e584be6b99495b64988e3f9c849371a8b
Alex Silva <alexcssilva@gmail.com>
2022-05-23 21:56:02 -0300

    feat: added - models/category.js - Desafio 07

---
+++ src/database/models/category.js
@@ -0,0 +1,19 @@
+
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+  }, {
+    timestamps: false
+  });
+
+  return Category;
+}

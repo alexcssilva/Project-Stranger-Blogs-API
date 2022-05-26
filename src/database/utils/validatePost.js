715c3a01bf2f81b22239b632b859e086374812da
Alex Silva <alexcssilva@gmail.com>
2022-05-26 19:03:05 -0300

    feat: added validatePost utils/validatePost - Desafio 12

---
+++ src/database/utils/validatePost.js
@@ -0,0 +1,12 @@
+const validatePost = async (req, res, next) => {
+  const { title, content, categoryIds } = req.body;
+
+  if (!title || !content || categoryIds.length === 0) {
+    return res.status(400)
+    .json({ message: 'Some required fields are missing' });
+  }
+
+ next();
+};
+
+module.exports = validatePost;
